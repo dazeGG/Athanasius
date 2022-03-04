1c60254341a1e096ba409f8ff3396f52c82d6e85
daze <vogisa@mail.ru>
2022-03-04 19:02:32 +0300

    focus mode for use

--- bot/scripts.py
+++ bot/scripts.py
@@ -231,11 +231,9 @@ async def message_to_all(game: {}, player: {}, asked_player: {}, request: str = 
                 _player['settings']['focus-mode-messages'][game["title"]] = \
                     _player['settings']['focus-mode-messages'].get(game["title"], '') + m + '\n'
                 mongo_users.update_one({'_id': _player['_id']}, {'$set': {'settings': _player['settings']}})
-            '''else:
+            else:
                 m = f'Игра: <b>{game["title"]}</b> - ' + m
-                await bot.send_message(_player['_id'], m)'''
-            m = f'Игра: <b>{game["title"]}</b> - ' + m
-            await bot.send_message(_player['_id'], m)
+                await bot.send_message(_player['_id'], m)
 
     with open('log.txt', 'a', encoding="utf-8") as file:
         file.write(message.replace('<b>', '').replace('</b>', '') + '\n')
