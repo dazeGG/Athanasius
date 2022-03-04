736c04749018b70a1196374092acbaa1e3f93ed3
daze <vogisa@mail.ru>
2022-03-04 18:02:14 +0300

    mongo_users -> users (config.py)

--- bot/config.py
+++ bot/config.py
@@ -9,9 +9,9 @@ from bot.config_reader import load_config
 
 config = load_config("config/bot.ini")
 
-bot = Bot(token=config.Athanasius_bot.token, parse_mode=ParseMode.MARKDOWN)
+bot = Bot(token=config.Athanasius_bot.token, parse_mode=ParseMode.MARKDOWN_V2)
 dp = Dispatcher(bot=bot, storage=MemoryStorage())
 
 collection = MongoClient(config.Athanasius_bot.cluster_link)['athanasius']
-mongo_users = collection['mongo_users']
-mongo_games = collection['mongo_games']
+mongo_users = collection['users']
+mongo_games = collection['games']
